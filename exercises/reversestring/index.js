b9ea6701ff36692fbf97045c9087e00b32003d50
thenatural86 <philip.smalls86@gmail.com>
2020-01-20 13:04:38 -0500

    reducer solution

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,12 +7,7 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  let reversed = ""
-
-  for (let character of str) {
-    reversed = character + reversed
-  }
-  return reversed
+  return str.split("").reduce((rev, char) => char + rev, "")
 }
 
 module.exports = reverse
@@ -22,4 +17,13 @@ module.exports = reverse
 //     .split("")
 //     .reverse()
 //     .join("")
+// }
+
+// function reverse(str) {
+//   let reversed = ""
+
+//   for (let character of str) {
+//     reversed = character + reversed
+//   }
+//   return reversed
 // }
